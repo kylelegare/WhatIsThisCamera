678a53219b141fcdc4f18bf5e9c4b0e092af7c57
Kyle Legare <kylelegare@gmail.com>
2024-05-14 00:28:28 +0000

    reverted back to libcamera

--- camera.py
+++ camera.py
@@ -1,15 +1,8 @@
 # camera.py
-from picamera2 import Picamera2
-from time import sleep
+import subprocess
 
 def capture_image(image_path):
     try:
-        picam2 = Picamera2()
-        config = picam2.create_still_configuration(main={"size": (800, 600)}, display=None)  # Disable preview window
-        picam2.configure(config)
-        picam2.start()
-        sleep(1)  # Let the camera warm up (you can adjust this value)
-        picam2.capture_file(image_path)
-        picam2.stop()
-    except Exception as e:
+        subprocess.run(['libcamera-still', '-o', image_path, '--width', '800', '--height', '600'], check=True)
+    except subprocess.CalledProcessError as e:
         print(f"An error occurred while capturing the image: {e}")
