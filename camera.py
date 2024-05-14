52ac5fe88f09fa4e4c9ade35f6d1e9c0d885fed8
Kyle Legare <kylelegare@gmail.com>
2024-05-14 00:38:02 +0000

    More settings to speed up photo

--- camera.py
+++ camera.py
@@ -6,11 +6,13 @@ def capture_image(image_path):
         subprocess.run([
             'libcamera-still',
             '-o', image_path,
-            '--width', '800', '--height', '600',
-            '--shutter', '30000',    # Fixed exposure time (30,000 microseconds)
-            '--gain', '4',           # Fixed analog gain
-            '--awbgains', '1,1',     # Fixed automatic white balance gains
-            '--framerate', '30'      # Fixed frame rate
+            '--width', '800', '--height', '600',  # Set resolution
+            '--shutter', '30000',                 # Fixed exposure time (30,000 microseconds)
+            '--gain', '4',                        # Fixed analog gain
+            '--awbgains', '1,1',                  # Fixed automatic white balance gains
+            '--framerate', '30',                  # Fixed frame rate
+            '--nopreview',                        # Disable preview
+            '--immediate'                         # Capture immediately
         ], check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while capturing the image: {e}")
